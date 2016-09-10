881b20f0840694b9d5623284ce342c67d89efa70
Brandon Kelley <14bmkelley@gmail.com>
2016-09-10 15:46:54 -0700

    Adding ProbabilityAI and commenting AI interface

--- src/org/spdcalpoly/AI.java
+++ src/org/spdcalpoly/AI.java
@@ -3,17 +3,32 @@ package org.spdcalpoly;
 import net.sf.javaml.core.Dataset;
 
 /**
- * Created by Alex on 9/10/2016.
+ * AI.java
+ *
+ * The AI interface is used to define the basic functionality of an AI. Its
+ * methods are called by the Game class when using the AI.
+ *
+ * @author Alex Boyd
+ * @since 09/10/2016
+ * @version 1.0
  */
 public interface AI {
 
-    // Read in training data from past collections
+    /**
+     * Read in training data from past collections.
+     * @return A dataset of the training data.
+     */
     Dataset generateData();
 
-    // Stochastically update the data from the current game
+    /**
+     * Stochastically update the data from the current game.
+     */
     void updateData();
 
-    // Run the current model and make a prediction
+    /**
+     * Run the current model and make a prediction.
+     * @return The prediction string.
+     */
     String runModel();
 
 }
---
+++ src/org/spdcalpoly/ProbabilityAI.java
@@ -0,0 +1,41 @@
+package org.spdcalpoly;
+
+import net.sf.javaml.core.Dataset;
+
+/**
+ * ProbabilityAI.java
+ *
+ * The ProbabilityAI class is used to make decisions based on probabilities
+ * drawn from the game's data.
+ */
+public class ProbabilityAI implements AI {
+    
+    /**
+     * Read in training data from past collections.
+     *
+     * @return A dataset of the training data.
+     */
+    @Override
+    public Dataset generateData() {
+        return null;
+    }
+
+    /**
+     * Stochastically update the data from the current game.
+     */
+    @Override
+    public void updateData() {
+
+    }
+
+    /**
+     * Run the current model and make a prediction.
+     *
+     * @return The prediction string.
+     */
+    @Override
+    public String runModel() {
+        return null;
+    }
+
+}
